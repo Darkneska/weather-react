119a11a1c2d91b2a924e013de7125a10e9764221
Darkneska <84958830+Darkneska@users.noreply.github.com>
2021-09-20 16:00:40 +0200

    remove footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@
 import React from "react";
 import './App.css';
 import Search from "./Search";
-import Footer from "./Footer"
+
 
 export default function App(){
   return (
     <div className="App">
       <div className="container">
         <Search  defaultCity="New York" />
-        <Footer />
+        <footer><a
+        href="https://github.com/Darkneska/weather-react"
+        target="_blank"
+        rel="noreferrer"
+        className="source"
+      >
+        Open-sourced on GitHub
+      </a>{" "}
+      by Denisa Pavlikova{" "}
+      <a
+        href="https://confident-kalam-ccc5df.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+        className="source"
+      >
+        hosted on Netlify
+      </a></footer>
       </div>
     </div>
   );
